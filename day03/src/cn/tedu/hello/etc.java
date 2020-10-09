73255e03b42738e489a3423684664e1a960227f9
watcher018 <lyr14380230@163.com>
2020-10-09 16:58:12 +0800

    第三天

--- day03/src/cn/tedu/hello/etc.java
+++ day03/src/cn/tedu/hello/etc.java
@@ -6,8 +6,10 @@ public class etc {
     public static void main(String[] args) {
         int a=new Scanner(System.in).nextInt();
         int b=new Scanner(System.in).nextInt();
+        int c=new Scanner(System.in).nextInt();
 
-        int max=a>b?a:b;
+        int max= Math.max(a, b);
+        max= Math.max(max, c);
         System.out.println(max);
     }
 }
