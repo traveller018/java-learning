dbd7b529d91957b028d57f558a69c83ca0418a87
watcher018 <lyr14380230@163.com>
2020-10-09 16:38:43 +0800

    再次测试

--- day03/src/cn/tedu/hello/etc.java
+++ day03/src/cn/tedu/hello/etc.java
@@ -1,2 +1,13 @@
-package cn.tedu.hello;public class etc {
+package cn.tedu.hello;
+
+import java.util.Scanner;
+
+public class etc {
+    public static void main(String[] args) {
+        int a=new Scanner(System.in).nextInt();
+        int b=new Scanner(System.in).nextInt();
+
+        int max=a>b?a:b;
+        System.out.println(max);
+    }
 }
